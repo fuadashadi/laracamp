2a15d09d8bf32a0ed8e373df9ee994e8b1f38b19
itfuad <fuadashadi2023@gmail.com>
2024-06-04 23:53:43 +0700

    Integration email after register and after checkout

--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,24 @@ Route::get('/', function () {
 //     return view('checkout');
 // })->name('checkout');
 
-Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
-Route::get('/checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
-Route::post('/checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+// socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.login.callback');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    Route::get('dashboard/checkout/invoice/{checkout}', [CheckoutController::class, 'invoice'])->name('user.checkout.invoice');
+});
+
 
 // Route::get('/success-checkout', function () {
 //     return view('success_checkout');
 // })->name('success-checkout');
 
-// socialite routes
-Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
-Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.login.callback');
-
-Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
